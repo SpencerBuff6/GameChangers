f0ac01b89de3daab88a397e1614747a4b77fe289
SpencerBuff6633 <61481885+SpencerBuff6633@users.noreply.github.com>
2022-08-07 19:55:15 -0600

    Register page possibly finished

--- GameChangers/GameChangers/Register.php
+++ GameChangers/GameChangers/Register.php
@@ -83,6 +83,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -90,4 +91,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
         <title>Sign Up</title>
         <link rel="stylesheet" href="">
     </head>
+    <body>
+        <div class="wrapper">
+            <h2>Sign Up</h2>
+            <p>Please fill this form to create an account.</p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control 
+                        <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>"/>
+                    <span class="invalid-feedback">
+                        <?php echo $username?>
+                    </span>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control
+                           <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" />
+                    <span class="invalid-feedback">
+                        <?php echo $password_err; ?>
+                    </span>
+                </div>
+                <div class="form-group">
+                    <label>Confirm Password</label>
+                    <input type="password" name="confirm_password" class="form-control
+                           <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirn_password; ?>"/>
+                    <span class="invalid_feedback">
+                        <?php echo $confirm_password_err ?>
+                    </span>
+                </div>
+                <div class="form-group">
+                    input type="submit" class="btn btn-primary" value="submit">
+                    input type="reset" class="btn btn-secondary ml-2" value="Reset">
+                </div>
+                <p>Already have an account? <a href="login.php>Login Here"</a>.</p>
+            </form>
+        </div>
+    </body>
 </html>
